fdce0d52d2586d6ac29e8a4efa860f34e69c2630
Ruslan <Ruslan90.0843@gmail.com>
2023-02-12 23:06:47 +0300

    added skeleton this application used Spring Boot

---
+++ src/test/java/com/example/JRTelegramBot/JrTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JRTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
